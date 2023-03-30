89b8d2485f2c791e31d45bd84fb8eb359301d3c1
Hashimjasem <you@example.com>
2023-03-31 01:02:35 +0800

    feched and pased data from api

---
+++ main.js
@@ -0,0 +1,21 @@
+import { all } from "axios";
+import "./style.css"
+import { getWeather } from "./weather";
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+.then(renderWeather)
+.catch(e => {
+    console.error(e)
+    alert("error getting weather.")
+})
+
+function renderWeather({current, daily, hourly}) {
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+function renderCurrentWeather(current) {
+    document.querySelector("[data-current-temp]").textContent = current.currentTemp
+}
